352851b748018e01461c570c37a65d061d0ca2c5
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-02-08 10:18:55 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+
+function* gettingStarted() {
+  yield <h1>Step 1</h1>;
+  yield <h1>Step 2</h1>;
+  yield <h1>Step 3</h1>;
+  yield <h1>Step 4</h1>;
+  yield <h1>Step 5</h1>;
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gettingStarted: gettingStarted(),
+      currentStep: null
+    };
+    this.nextItem = this.nextItem.bind(this);
+  }
+
+  nextItem() {
+    this.setState({
+      currentStep: this.state.gettingStarted.next().value
+    });
+  }
+
+  componentDidMount() {
+    this.nextItem();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.state.currentStep ? (
+            <Fragment>
+              <div>{this.state.currentStep}</div>
+              <div>
+                <button onClick={this.nextItem}>Next &raquo;</button>
+              </div>
+            </Fragment>
+          ) : (
+            <div>Fin</div>
+          )}
         </header>
       </div>
     );
